b96775834fba4f5fff4560eff208742df45f4988
yash mantri <mantriyash2025@gmail.com>
2020-06-10 17:08:04 +0530

    soln

---
+++ SearchInsertPosition.js
@@ -0,0 +1,8 @@
+var searchInsert = function(nums, target) {
+    if(target > nums[nums.length - 1]) return nums.length
+    for(let i = 0; i < nums.length; i++) {
+        if(target <= nums[i]) {
+            return i
+        }
+    }
+};
