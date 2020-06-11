0aacfb4940e5c836f45de9838d8bddd57f00dba5
yash mantri <mantriyash2025@gmail.com>
2020-06-11 13:23:00 +0530

    soln

---
+++ SortColors.js
@@ -0,0 +1,25 @@
+var sortColors = function(nums) {
+    
+    function swap(i, j) {
+    [nums[i], nums[j]] = [nums[j], nums[i]];
+  }
+
+  let l = 0;
+  let r = nums.length - 1;
+  let i = 0;
+
+  while (i <= r) {
+    const n = nums[i];
+    if (n === 0) {
+      swap(i, l);
+      l++;
+      i++;
+    } else if (n === 2) {
+      swap(i, r);
+      r--;
+    } else {
+      i++;
+    }
+  }
+    
+};
